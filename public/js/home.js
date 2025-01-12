212c265d93b9e0fc3a66ace3ab4f58f5da8daf96
Kaique <kiaqbs@gmail.com>
2025-01-12 17:35:23 -0300

    validations - completed

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,55 @@
-const inputs = document.getElementsByTagName('input')
-form = document.getElementById('form')
+form = document.getElementById('form');
+const userInput = document.getElementById('user');
+const subjectInput = document.getElementById('subject');
+const postInput = document.getElementById('addpost');
 
-function addErro(input) {
-    input.classList.add('erro')
-}
+function addError(input) {
+    input.classList.add('error');
+};
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('oi')
-})
+    e.preventDefault()
+
+    checkUserinput(); 
+    checkSubjectinput();
+    checkPostinput();
+
+    if( checkUserinput() || checkSubjectinput() || checkPostinput()) {
+        form.submit();
+    } else {
+        console.log('Campos não foram preenchidos')
+    }
+});
+
+function checkUserinput() {
+    userInputValue = userInput.value;
+    if(userInputValue === "") {
+        addError(userInput);
+        return false
+    } else {
+        userInput.classList.remove('error');
+        return true
+    };
+};
+
+function checkSubjectinput() {
+    subjectInputValue = subjectInput.value;
+    if(subjectInputValue === "") {
+        subjectInput.classList.add('error');
+        return false;
+    } else {
+        subjectInput.classList.remove('error');
+        return true
+    };
+};
+
+function checkPostinput() {
+    postInputValue = postInput.value;
+    if(postInputValue === "") {
+        postInput.style.border = '1px solid red';
+        return false;
+    } else {
+        postInput.style.border = 'none'
+        return true
+    };
+};
