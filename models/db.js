f4b54d3fba9b5706a33cbfb5d9ed85efd6ccff8b
Kaique <kiaqbs@gmail.com>
2025-01-12 23:58:01 -0300

    database connection - created

---
+++ models/db.js
@@ -0,0 +1,12 @@
+// Sequelize configuration
+
+const { query } = require('express');
+const Sequelize = require('sequelize');
+
+// connection to mysql
+const sequelize = new Sequelize('crud_posts', 'root', 'Kiaq7900', {
+    host: "localhost",
+    dialect: "mysql",
+    query: { raw: true }
+});
+
