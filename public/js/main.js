0602a75dc7de296056e05f334fc1b83049479689
Kaique <kiaqbs@gmail.com>
2025-01-15 12:37:01 -0300

    update - developing

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
+const topPage = document.getElementById('top-page')
 const nav = document.querySelector('.nav');
 const hamburger = document.querySelector('.btn-hamburger');
+
+topPage.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+})
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('active')
