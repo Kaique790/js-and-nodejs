3d663b6229cae7ea2d6f17ecd3001d1aac14726c
Kaique <kiaqbs@gmail.com>
2025-01-26 10:44:11 -0300

    installed dependencies

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 8081
+const exphbs = require('express-handlebars');
+
+const PORT = 8081;
+
+// config handlebars
 
 
-app.listen(PORT, console.log('Servidor ligado na porta: ',PORT))
+
+
+app.listen(PORT, console.log('Servidor ligado na porta: ',PORT));
