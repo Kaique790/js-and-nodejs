43f0489920d4cd6c154499ffdb229c791ac7dc34
Kaique <kiaqbs@gmail.com>
2025-01-29 15:16:47 -0300

    API - finalized

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const { mongooose, default: mongoose } = require("mongoose");
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+});
+
+
+module.exports = User
