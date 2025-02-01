05481f287c62ff9cd784c786e45cec9f0e10b997
Kaique <kiaqbs@gmail.com>
2025-01-31 23:54:11 -0300

    gulp - started

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const series = gulp.series;
+
+function copiar (cb) {
+    console.log('Task de copiar...')
+
+    return cb()
+}
+
+module.exports.default = series(copiar)
