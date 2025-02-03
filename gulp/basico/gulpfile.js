e456d968d3e64bafac00194d58316b2dac73fcf5
Kaique <kiaqbs@gmail.com>
2025-02-03 00:26:28 -0300

    Update gulpfile.js

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,10 +1,30 @@
 const gulp = require('gulp');
-const series = gulp.series;
+const { series } = gulp;
 
+function antes1 (cb) {
+    console.log('Task de antes1...')
+
+    return cb()
+}
+function antes2 (cb) {
+    console.log('Task de antes2...')
+
+    return cb()
+}
 function copiar (cb) {
     console.log('Task de copiar...')
 
     return cb()
 }
+function fim (cb) {
+    console.log('fim...')
 
-module.exports.default = series(copiar)
+    return cb()
+}
+
+module.exports.default = series(
+    antes1,
+    antes2,
+    copiar,
+    fim,
+)
