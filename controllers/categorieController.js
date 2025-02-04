2c77c4d437c1581480d579fe230bd2fbebfb688c
Kaique <kiaqbs@gmail.com>
2025-02-03 21:05:34 -0300

    add categories - in progress

--- controllers/categorieController.js
+++ controllers/categorieController.js
@@ -1,5 +1,17 @@
 const Categorie = require('../models/Categorie');
 
 const addCategorie = async (req, res) => {
-    // Logic
+    const name = req.body.name
+    if (!name) return res.status(400).json({ msg: 'Informe um nome para a categoria' });
+
+    const categorie = await Categorie.findOne({ name });
+    if (categorie) return res.status(409).json({ msg: 'Categoria já cadastrada' });
+
+    try {
+        const newCategorie = new Categorie({ name });
+        await newCategorie.save();
+        res.status(201).json({ msg: 'Nova categoria adicionada!' });
+    } catch (err) {
+        res.status(500).json({ msg: 'Não foi possivel salvar a categoria devido a um erro interno' });
+    }
 }
