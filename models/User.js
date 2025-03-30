dd414dd6c96eed4f856249969eda35b8334e42f4
Kaique790 <kiaqbs@gmail.com>
2025-03-29 23:18:49 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,24 +1,24 @@
-import mongo from 'mongoose'
+import mongo from "mongoose";
 const model = mongo.model;
 
-const User = model('User', {
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        default: 'user',
-        enum: ['user', 'admin']
-    }
+const User = model("User", {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "admin"],
+  },
 });
 
 export default User;
